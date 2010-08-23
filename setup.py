521cf5b14c16bfc102df72d72ef9536cc97f7771
lucie <lucie@sixfeetup.com>
2010-08-23 14:42:36 +0000

    * added a version.txt file so it is read by portal_quickinstaller,  * updated setup.py to read this new version.txt file,  * updated configure.zcml so the egg is considered as a package,  * removed old code from init.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.2dev'
+version = open(os.path.join("sixfeetup", "customfolderalert", "version.txt")).read()
 
 setup(name='sixfeetup.customfolderalert',
       version=version,
--- sixfeetup/customfolderalert/__init__.py
+++ sixfeetup/customfolderalert/__init__.py
@@ -1,5 +1,2 @@
   # -*- extra stuff goes here -*-
-from Products.PythonScripts.Utility import allow_module
-
-allow_module("os.environ")
 
