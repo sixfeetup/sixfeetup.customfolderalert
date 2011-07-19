cec32195edc5ebd62f2078d915ce8194cb530d06
Clayton Parker <me@claytron.com>
2011-07-19 02:56:53 +0000

    Move everything to the `alert_view` view class
     - Remove `evilchecking` python script and move logic into the view class
     - Remove alert view interface
     - Add `allowed_attributes` for the `AlertView`
     - Change naming of methods to match PEP8

--- sixfeetup/customfolderalert/browser/alertview.py
+++ sixfeetup/customfolderalert/browser/alertview.py
@@ -1,16 +1,29 @@
 import os
+from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
-from zope.interface import implements
-from sixfeetup.customfolderalert.browser.interfaces import IAlertView
+
 
 class AlertView(BrowserView):
     """View class for alerts methods"""
-    implements(IAlertView)
 
-    def isAlertDisabled(self):
+    def is_alert_disabled(self):
         """Is the custom folder alert disabled?"""
         TRUISMS = ['yes', 'y', 'true', 'on']
         DISABLED = os.environ.get("DISABLE_CUSTOMFOLDER_ALERT", None)
 
         return DISABLED is not None and DISABLED.lower() in TRUISMS
 
+    def check_for_evil(self):
+        """Check to see if any non filesystem directories have
+        any items.
+        """
+        stool = getToolByName(self.context, 'portal_skins')
+        skin_folders = stool.objectIds()
+        # loop through all the skins dirs
+        for folder in skin_folders:
+            folder = stool[folder]
+            # only return true if it's not a fs dir and has something in it
+            zope_folder = folder.meta_type != 'Filesystem Directory View'
+            if zope_folder and folder.objectIds():
+                return True
+        return False
--- sixfeetup/customfolderalert/browser/interfaces.py
+++
@@ -1,8 +0,0 @@
-from zope.interface import Interface
-
-class IAlertView(Interface):
-    """View class for alerts methods"""
-
-    def isAlertDisabled():
-        """Is the custom folder alert disabled?"""
-
